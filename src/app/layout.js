2a038cf34be87ab777c8f0edaf6e3c192e947b3e
uzimp q <worrakit.boss@gmail.com>
2024-04-22 17:15:31 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Noto_Sans_Thai } from "next/font/google";
 import { Noto_Sans_JP } from "next/font/google";
 import { Noto_Sans_KR } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const hankenGroteskFont = Hanken_Grotesk({
@@ -17,7 +18,8 @@ export const metadata = {
   title: "Worakrit K.",
   description:
     "Passionate problem-solver, designer, and idea generator with a drive to take them to the next level. Located in Thailand.",
-  image: "./dog.jpg",
+  image:
+    "https://i.pinimg.com/originals/8f/98/02/8f980249b56c4fdde948933bb3b8057a.jpg",
 };
 
 export default function RootLayout({ children }) {
@@ -25,6 +27,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={hankenGroteskFont.className}>
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
